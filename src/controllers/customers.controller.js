7baca25b10f1c3c8c216315acb3d442503ae2c4a
Guilherme <figueiredoguiu@gmail.com>
2023-05-12 09:02:19 -0300

    Feat: Update de cliente

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -48,3 +48,37 @@ export async function postCustomer(req, res) {
         res.status(500).send(err.message);
     }
 };
+
+export async function putCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+    const { id } = req.params;
+
+    try {
+        const existingCustomerResult = await db.query(`SELECT id, cpf FROM customers WHERE id = $1;`, [id]);
+
+        if (existingCustomerResult.rowCount === 0) {
+            return res.status(404).send(`Cliente não encontrado.`);
+        }
+
+        const existingCustomer = existingCustomerResult.rows[0];
+        const existingCpf = existingCustomer.cpf;
+
+        if (cpf && existingCpf !== cpf) {
+            const duplicateCpfResult = await db.query(`SELECT id FROM customers WHERE cpf = $1;`, [cpf]);
+
+            if (duplicateCpfResult.rowCount > 0) {
+                return res.status(409).send(`O CPF já pertence a outro cliente.`);
+            }
+        }
+
+        await db.query(`
+            UPDATE customers
+            SET name = $1, phone = $2, birthday = $3, cpf = $4
+            WHERE id = $5;
+        `, [name, phone, birthday, cpf, id])
+
+        res.status(200).send(`Dados do cliente atualizados com sucesso`)
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCustomerById, getCustormers, postCustomer } from "../controllers/customers.controller.js";
+import { getCustomerById, getCustormers, postCustomer, putCustomer } from "../controllers/customers.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { customerSchema } from "../schemas/customers.schemas.js";
 
@@ -7,6 +7,7 @@ const customersRouter = Router();
 
 customersRouter.get("/customers", getCustormers);
 customersRouter.get("/customers/:id", getCustomerById);
-customersRouter.post("/customers", validateSchema(customerSchema), postCustomer)
+customersRouter.post("/customers", validateSchema(customerSchema), postCustomer);
+customersRouter.put("/customers/:id", validateSchema(customerSchema), putCustomer);
 
 export default customersRouter;
